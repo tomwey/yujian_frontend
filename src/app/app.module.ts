cc2debadf534a2cb07f00ac74988c305fa0660f0
Tomwey <tomwey@163.com>
2017-04-20 17:32:44 +0800

    update some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { ExplorePage } from '../pages/explore/explore';
+import { SettingPage } from '../pages/setting/setting';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -14,8 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ExplorePage,
+    SettingPage,
     HomePage,
     TabsPage
   ],
@@ -26,8 +26,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ExplorePage,
+    SettingPage,
     HomePage,
     TabsPage
   ],
--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-setting',
+  templateUrl: 'setting.html'
 })
-export class ContactPage {
+export class SettingPage {
 
   constructor(public navCtrl: NavController) {
 
