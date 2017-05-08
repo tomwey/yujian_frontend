65ac9e1309a5476251b53b17dd0d80120a0aaca2
Tomwey <tomwey@163.com>
2017-05-08 14:19:55 +0800

    ccc

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EventsService } from '../../providers/events-service';
+import { ToolService } from '../../providers/tool-service';
 
 /**
  * Generated class for the EventDetail page.
@@ -13,13 +15,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'event-detail.html',
 })
 export class EventDetailPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log(this.navParams.data);
+  event: any = null;
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private events: EventsService,
+              private toolService: ToolService) {
+    // console.log(this.navParams.data);
+    this.event = this.navParams.data;
   }
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad EventDetail');
+    setTimeout(() => {
+      this.loadEvent();
+    }, 100);
+  }
+
+  loadEvent(): void {
+    this.toolService.showLoading();
+
+    this.events.getEvent(this.event.id)
+      .then(data => {
+        this.event = data;
+        this.toolService.hideLoading();
+      })
+      .catch(error => {
+        this.toolService.hideLoading();
+        setTimeout(() => {
+          this.toolService.showToast(error);
+        }, 200);
+      });
   }
 
 }
