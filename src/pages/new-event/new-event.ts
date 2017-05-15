4a2c0898e1eae146ff8883bacc787effbe00ce3c
Tomwey <tomwey@163.com>
2017-05-15 13:08:14 +0800

    add some func

--- src/pages/new-event/new-event.ts
+++ src/pages/new-event/new-event.ts
@@ -1,12 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, Renderer } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the NewEvent page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-new-event',
@@ -14,29 +8,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NewEventPage {
 
-  hbType: string = '0';
-  eventType: string = 'Quiz';
-  multiple: string = '';
-  btnIcon: string = 'ios-add-outline';
-  uploadTip: string = '选择文件';
-  editorOptions: any = {};
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.editorOptions = {
-      placeholderText: '输入活动内容，支持图文混排！',
-      charCounterCount: false,
-    };
+  @ViewChild('fileInput') nativeFileInputBtn: ElementRef;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private renderer: Renderer) {
+    
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NewEvent');
+    // console.log('ionViewDidLoad NewEvent');
   }
 
-  uploadFiles(event: Event): void {
-    console.log(event);
+  uploadFile(): void {
+    let clickEvent: MouseEvent = new MouseEvent('click', { bubbles: true });
+    this.renderer.invokeElementMethod(
+      this.nativeFileInputBtn.nativeElement, "dispatchEvent", [clickEvent]
+    );
   }
 
-  changeEventType(): void {
-    // console.log(this.event_type);
+  selectedFiles(event: Event): void {
+    let files: FileList = this.nativeFileInputBtn.nativeElement.files;
+    console.log(files);
   }
 
 }
