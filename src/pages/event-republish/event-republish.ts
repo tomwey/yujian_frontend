a8da6fa18d229acbf779d62cdca9e6749c9b81df
Tomwey <tomwey@163.com>
2017-07-16 18:31:24 +0800

    add some func

--- src/pages/event-republish/event-republish.ts
+++ src/pages/event-republish/event-republish.ts
@@ -19,8 +19,10 @@ export class EventRepublishPage {
 
   hbId: number = null;
   hb: any = { type: '0', total_money: null, min_value: null, max_value: null, total: null, value: null };
+  share_hb: any = { type: '0', total_money: null, min_value: null, max_value: null, total: null, value: null };
   canCommit: boolean = false;
 
+  leftMoney: number = 0.00;
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private viewController: ViewController,
@@ -28,6 +30,10 @@ export class EventRepublishPage {
               private events: EventsService,
               ) {
     this.hbId = this.navParams.data.id;
+
+    if (this.navParams.data && this.navParams.data.shb) {
+      this.leftMoney = this.navParams.data.shb.left_money;
+    }
   }
 
   ionViewDidLoad() {
@@ -35,6 +41,8 @@ export class EventRepublishPage {
   }
 
   commit(): void {
+
+    // 广告红包
     let hb;
     if (this.hb.type === '0') {
       hb = { type: 0, 
@@ -46,6 +54,21 @@ export class EventRepublishPage {
              total: this.hb.total,
              value: this.hb.value};
     }
+
+    // 分享红包
+    let share_hb = null;
+    if (this.share_hb.type === '0') {
+      share_hb = { type: 0, 
+             total_money: this.share_hb.total_money,
+             min_value: this.share_hb.min_value, 
+             max_value: this.share_hb.max_value };
+    } else {
+      share_hb = { type: 1, 
+             total: this.share_hb.total,
+             value: this.share_hb.value};
+    }
+
+    hb.share_hb = share_hb
 
     this.tool.showLoading('提交中...');
 
@@ -61,11 +84,11 @@ export class EventRepublishPage {
       })
       .catch(error => {
         this.tool.hideLoading();
-
+        
         this.resetForm();
 
         setTimeout(() => {
-          this.tool.showToast('提交失败');
+          this.tool.showToast(error);
         }, 200);
       });
   }
@@ -114,6 +137,53 @@ export class EventRepublishPage {
       if (this.hb.total && this.hb.total > 0) {
 
         return this.hb.total.toString();
+      } else {
+
+        return '--';
+      }
+    }
+  }
+
+  calcuTotalMoney2(): string {
+    if (this.share_hb.type === '0') {
+      if (this.share_hb.total_money && this.share_hb.total_money > 0) {
+        return `${this.share_hb.total_money}元`;
+      } else {
+        return '--'
+      }
+    } else {
+      if (this.share_hb.total && this.share_hb.value) {
+        let totalValue = this.share_hb.total * this.share_hb.value;
+        return totalValue > 0 ? `${totalValue}元` : '--'; 
+      } else {
+        return '--';
+      }
+    }
+  }
+
+  calcuTotal2(): string {
+    if (this.share_hb.type === '0') {
+      // 随机红包
+      if (this.share_hb.total_money && this.share_hb.total_money > 0 &&
+          this.share_hb.min_value && this.share_hb.min_value > 0 && 
+          this.share_hb.max_value && this.share_hb.max_value > 0 && 
+          this.share_hb.min_value < this.share_hb.max_value) {
+            let min = Math.floor(this.share_hb.total_money / this.share_hb.max_value);
+            let max = Math.floor(this.share_hb.total_money / this.share_hb.min_value);
+
+            if (min <= 0) {
+              return '--';
+            }
+
+            return `${min} - ${max}`;
+          } else {
+            return '--';
+          }
+    } else {
+      // 固定红包
+      if (this.share_hb.total && this.share_hb.total > 0) {
+
+        return this.share_hb.total.toString();
       } else {
 
         return '--';
