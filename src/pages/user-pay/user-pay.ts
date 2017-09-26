bf3f636cca40507e4b845aa32e27a2c20f9320ee
tomwey <tomwey@163.com>
2017-09-26 13:44:43 +0800

    change user pay func

--- src/pages/user-pay/user-pay.ts
+++ src/pages/user-pay/user-pay.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, IonicPage } from 'ionic-angular';
 import { UserService } from '../../providers/user-service';
 import { ToolService } from '../../providers/tool-service';
+import { DomSanitizer } from '@angular/platform-browser';
 
+import { Md5 } from 'ts-md5/dist/md5';
 /**
  * Generated class for the UserPayPage page.
  *
@@ -17,24 +19,27 @@ import { ToolService } from '../../providers/tool-service';
 export class UserPayPage {
 
   user_pay: any = { money: null };
-  payData: any;
+  payData: any = null;
+  secUrl: any;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private users: UserService,
     private toolService: ToolService,
+    private sanitizer: DomSanitizer,
   ) {
   }
 
   commit(): void {
-    this.toolService.showLoading('正在提交申请...');
+    this.toolService.showLoading('正在提交...');
     this.users.applyPay(this.user_pay.money)
       .then(data => {
-        this.toolService.hideLoading();
+        // this.toolService.hideLoading();
         this.payData = data;
 
-        this.gotoPayQrcode();
+        this.requestQrcode();
+        // this.showPayQrcode();
       })
       .catch(error => {
         this.toolService.hideLoading();
@@ -44,8 +49,26 @@ export class UserPayPage {
       });
   }
 
-  gotoPayQrcode(): void {
-    this.navCtrl.push('UserPayQrcodePage', this.payData);
+  refresh(): void {
+    this.toolService.showLoading('刷新中...');
+
+    this.requestQrcode();
+  }
+
+  requestQrcode(): void {
+    let i  = new Date().getTime();
+    let ak = this.generateAccessKey(i);
+    
+    let url = this.payData.pay_url + `?code=${this.payData.code}&i=${i}&ak=${ak}`;
+    this.secUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  generateAccessKey(i): string {
+    return Md5.hashStr(this.payData.code.toString() + i.toString() + '3666a3c050d539be71da0f3b283eb433', false).toString();
+  }
+
+  loaded(): void {
+    this.toolService.hideLoading();
   }
 
 }
