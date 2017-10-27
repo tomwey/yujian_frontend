8418d6da8d443dfac4c15bff0e86f98535cf713d
tomwey <tomwey@163.com>
2017-10-27 14:09:29 +0800

    change app version

--- src/providers/api-service.ts
+++ src/providers/api-service.ts
@@ -18,7 +18,7 @@ const API_KEY:  string = "3bde7fa81ca14a5d9f829bcae9c72c67";
 // const API_HOST: string = "http://0.0.0.0:8888/api/v1";
 // const API_KEY:  string = "1e3bb5a6e93148d7a6aa20ce181c1c46";
 
-export const APP_VERSION = '3.4';
+export const APP_VERSION = '3.5';
 
 @Injectable()
 export class ApiService {
